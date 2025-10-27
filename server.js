039eda285e33d21f703c800fb39af3462e39399e
Joseph Brian <natividadjosephbrian@gmail.com>
2025-10-27 12:03:48 +0800

    feat(debug): Fix MONGO_URI spellings

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = "../config/db.config";
+const connectDB = require("./src/config/db.config");
 
 dotenv.config();
-
+console.log('Is MONGO_URI loaded?', process.env.MONGO_URI);
 const app = express();
 
+// Create config folder and db config first
 connectDB();
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // enabling Cross-Over Resourse Sharing
+app.use(express.json()); // allow the app to accept JSON data
 
+// a simple test route
 app.get("/", (req, res) => {
-  req.setEncoding("API is running...");
+  res.send("API is running...");
 });
+
+// API Routes
